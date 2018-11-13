c069adf3d43286cca44798d8f891926d9be26a4c
J. B. Rainsberger <me@jbrains.ca>
2018-11-13 14:41:17 +0100

    We can now choose different prices for different barcodes.

--- PointOfSale-Java/src/test/java/ca/jbrains/pos/test/SellOneItemTest.java
+++ PointOfSale-Java/src/test/java/ca/jbrains/pos/test/SellOneItemTest.java
@@ -1,27 +1,49 @@
 package ca.jbrains.pos.test;
 
 import org.junit.Assert;
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class SellOneItemTest {
     @Test
     public void productFound() throws Exception {
         Display display = new Display();
-        Sale sale = new Sale();
+        Sale sale = new Sale(display);
 
         sale.onBarcode("12345");
 
         Assert.assertEquals("EUR 7.50", display.getText());
     }
+    @Ignore("refactoring")
+    @Test
+    public void anotherProductFound() throws Exception {
+        Display display = new Display();
+        Sale sale = new Sale(display);
+
+        sale.onBarcode("23456");
+
+        Assert.assertEquals("EUR 12.95", display.getText());
+    }
 
     public static class Sale {
-        public void onBarcode(String barcode) {
+        private Display display;
 
+        public Sale(Display display) {this.display = display;}
+
+        public void onBarcode(String barcode) {
+            display.setText("EUR 7.50");
         }
     }
+
     public static class Display {
+        private String text;
+
         public String getText() {
-            return "EUR 7.50";
+            return text;
+        }
+
+        public void setText(String text) {
+            this.text = text;
         }
     }
 }
