e6a2ddbc8bca20de4efecbc799e6ab99ca9942d7
J. B. Rainsberger <me@jbrains.ca>
2018-11-14 15:51:00 +0100

    We can now format a Price.

--- PointOfSale-Java/src/test/groovy/ca/jbrains/pos/test/FormatPriceMessageInEnglishSpecification.groovy
+++ PointOfSale-Java/src/test/groovy/ca/jbrains/pos/test/FormatPriceMessageInEnglishSpecification.groovy
@@ -4,13 +4,22 @@ import spock.lang.Specification
 import spock.lang.Unroll
 
 class FormatPriceMessageInEnglishSpecification extends Specification {
-    @Unroll("#price formats as #text")
+    @Unroll("#priceInEuroCents eurocents formats as #text")
     def "format price messages"() {
+        def price = Price.euroCents(priceInEuroCents)
+
         expect:
         new EnglishLanguageFormatForEurope().formatPrice(price) == text
 
         where:
-        price                || text
-        Price.euroCents(795) || "EUR 7.95"
+        priceInEuroCents || text
+        795              || "EUR 7.95"
+        0                || "EUR 0.00"
+        1                || "EUR 0.01"
+        10               || "EUR 0.10"
+        12               || "EUR 0.12"
+        1691             || "EUR 16.91"
+        600              || "EUR 6.00"
+        12932384         || "EUR 129\u2009323.84"
     }
 }
--- PointOfSale-Java/src/test/java/ca/jbrains/pos/test/EnglishLanguageFormatForEurope.java
+++ PointOfSale-Java/src/test/java/ca/jbrains/pos/test/EnglishLanguageFormatForEurope.java
@@ -1,11 +1,28 @@
 package ca.jbrains.pos.test;
 
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+
 public class EnglishLanguageFormatForEurope implements MessageFormat {
+
+    private final DecimalFormat decimalFormat = createDecimalFormat();
+
     public EnglishLanguageFormatForEurope() {
     }
 
     @Override
     public String formatPrice(Price price) {
-        return String.format("EUR %.2f", price.inEuro());
+        return decimalFormat.format(price.inEuro());
+    }
+
+    private static DecimalFormat createDecimalFormat() {
+        DecimalFormatSymbols decimalFormatSymbols = new DecimalFormatSymbols();
+        decimalFormatSymbols.setGroupingSeparator('\u2009');
+
+        DecimalFormat decimalFormat = new DecimalFormat("EUR ############0.00");
+        decimalFormat.setGroupingSize(3);
+        decimalFormat.setGroupingUsed(true);
+        decimalFormat.setDecimalFormatSymbols(decimalFormatSymbols);
+        return decimalFormat;
     }
 }
